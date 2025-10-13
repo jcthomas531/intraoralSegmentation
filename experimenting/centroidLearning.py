83533ebe22372ef56baf10e880d8a704e52360ad
James C. Thomas <jcthomas531@gmail.com>
2025-10-13 10:02:34 -0500

    generic functions for reading and maniputlation of ply data

--- experimenting/centroidLearning.py
+++ experimenting/centroidLearning.py
@@ -99,8 +99,7 @@ uCols[16]
 
 
 
-#as a double check, lets make sure that none of the colors are the same in the
-#upper and lower jaw
+#data frames associating tooth number with color
 uNumCol = pd.DataFrame(
     {
      "toothNum": ["16","15","14","13","12","11","10","3","2","1","4","9","5","6","8","4","gum"],
@@ -122,6 +121,99 @@ lNumCol = pd.DataFrame(
     )
 
 
+#as a double check, lets make sure that none of the colors are the same in the
+a = uNumCol["color"].isin(lNumCol["color"])
+#only the gums color is in both
+
+aa = a.sum()
+aa
+
+
+
+#create a function that reads in a ply file and formats in how I like
+#this will require PlyData and pandas package
+def plyRead(file):
+    #read in the object
+    plyObject = PlyData.read(file)
+    #get the vertex and face data
+    plyVert = pd.DataFrame(plyObject["vertex"].data)
+    plyFace = pd.DataFrame(plyObject["face"].data)
+    #create new variable in face data for tooth color that concats RGB vals
+    plyFace["color"] = (plyFace["red"].astype(str).str.zfill(3) + "-" +
+                         plyFace["green"].astype(str).str.zfill(3) + "-" +
+                         plyFace["blue"].astype(str).str.zfill(3))
+    #return a dictionary of the vertex and face information
+    #this dictionary object seems like a named list in R
+    return {"vert": plyVert, "face": plyFace}
+
+
+
+
+os.chdir(dataPath+"Train-U")
+t1 = plyRead("076_U.ply")
+t1["vert"]
+t1["face"]
+pf.plotPly(face = t1["face"], vertex = t1["vert"])
+standOut(vertex=t1["vert"], face = t1["face"], toothCol="155-048-255")
+
+
+
+
+
+#function that adds tooth number to the face data frame
+#this takes a face data frame that has been set up using plyRead
+def toothNum(face):
+    #make copies of the dataframes so you dont edit in place
+    faceC = face.copy()
+    #color and tooth number associations
+    uNumCol = pd.DataFrame(
+        {
+         "toothNum": ["16","15","14","13","12","11","10","3","2","1","4","9","5","6","8","4","gum"],
+         "color": ['155-048-255', '255-099-071', '255-211-155','131-111-255','255-106-106',
+                   '060-179-113', '255-246-143', '255-000-255', '030-144-255', '000-255-127',
+                   '000-255-255', '127-255-000', '255-255-000', '000-255-000', '255-000-000',
+                   '000-000-255', '255-255-255']
+        }
+        )
+    lNumCol = pd.DataFrame(
+        {
+        "toothNum": ["25","24","26","23","27","22","28","21","29","20","30","19","31","18","32","17","gum"],
+        "color": ['139-000-000', '255-048-048', '144-238-144', '000-191-255', '000-139-139',
+                  '255-165-000', '000-000-139','202-255-112', '139-000-139', '200-255-255',
+                  '255-105-180', '255-228-255',  '230-230-250', '255-155-255', '255-228-181',
+                  '255-069-000', '255-255-255']
+        }
+        )
+    #identify if we have an upper or lower arch
+    #get the count of how many colors in this face data are in the colors of
+    #the upper face data frame. This will always be at least 1 bc of the gums
+    #if this value is above 1, then we have an upper arch, if this value is 1
+    #then we have a lower arch
+    upperColCount = uNumCol["color"].isin(faceC["color"]).sum()
+    if upperColCount > 1:
+        faceC = faceC.merge(uNumCol, on="color", how = "left", validate = "many_to_one")
+        faceC["arch"] = "upper"
+    elif upperColCount == 1:
+        faceC = faceC.merge(lNumCol, on="color", how = "left", validate = "many_to_one")
+        faceC["arch"] = "lower"
+    else:
+        ValueError("could not identify input as upper or lower arch")
+    #return updated dataframe
+    return faceC
+
+
+toothNum(t1["face"])["toothNum"].unique()
+
+os.chdir(dataPath+"train-Lall")
+t2 = plyRead("076_L.ply")
+toothNum(t2["face"])["toothNum"].unique()
+
+pf.plotPly(face = t2["face"], vertex = t2["vert"])
+
+
+
+#create a function that takes a data frame of faces, vertices, and tooth number
+#and shows that specific tooth
 
 
 #when you functionize the application of these numbers make sure to check every number
--- tools/plyFunctions.py
+++ tools/plyFunctions.py
@@ -1,10 +1,79 @@
 #helpful functions
 import pandas as pd
 import pyvista as pv
+from  plyfile import PlyData
 
 
 
-#plot face and vertex data from PlyData
+
+#function that reads in a ply file and formats in how I like
+#this will require PlyData and pandas package
+def plyRead(file):
+    #read in the object
+    plyObject = PlyData.read(file)
+    #get the vertex and face data
+    plyVert = pd.DataFrame(plyObject["vertex"].data)
+    plyFace = pd.DataFrame(plyObject["face"].data)
+    #create new variable in face data for tooth color that concats RGB vals
+    plyFace["color"] = (plyFace["red"].astype(str).str.zfill(3) + "-" +
+                         plyFace["green"].astype(str).str.zfill(3) + "-" +
+                         plyFace["blue"].astype(str).str.zfill(3))
+    #return a dictionary of the vertex and face information
+    #this dictionary object seems like a named list in R
+    return {"vert": plyVert, "face": plyFace}
+# import os
+# os.chdir("H:\\schoolFiles\\dissertation\\intraoralSegmentation\\fastTgcn\\data\\train-Lall")
+# l76 = plyRead("076_L.ply")
+
+
+#function that adds tooth number to the face data frame
+#this takes a face data frame that has been set up using plyRead
+def toothNum(face):
+    #make copies of the dataframes so you dont edit in place
+    faceC = face.copy()
+    #color and tooth number associations
+    uNumCol = pd.DataFrame(
+        {
+         "toothNum": ["16","15","14","13","12","11","10","3","2","1","4","9","5","6","8","4","gum"],
+         "color": ['155-048-255', '255-099-071', '255-211-155','131-111-255','255-106-106',
+                   '060-179-113', '255-246-143', '255-000-255', '030-144-255', '000-255-127',
+                   '000-255-255', '127-255-000', '255-255-000', '000-255-000', '255-000-000',
+                   '000-000-255', '255-255-255']
+        }
+        )
+    lNumCol = pd.DataFrame(
+        {
+        "toothNum": ["25","24","26","23","27","22","28","21","29","20","30","19","31","18","32","17","gum"],
+        "color": ['139-000-000', '255-048-048', '144-238-144', '000-191-255', '000-139-139',
+                  '255-165-000', '000-000-139','202-255-112', '139-000-139', '200-255-255',
+                  '255-105-180', '255-228-255',  '230-230-250', '255-155-255', '255-228-181',
+                  '255-069-000', '255-255-255']
+        }
+        )
+    #identify if we have an upper or lower arch
+    #get the count of how many colors in this face data are in the colors of
+    #the upper face data frame. This will always be at least 1 bc of the gums
+    #if this value is above 1, then we have an upper arch, if this value is 1
+    #then we have a lower arch
+    upperColCount = uNumCol["color"].isin(faceC["color"]).sum()
+    if upperColCount > 1:
+        faceC = faceC.merge(uNumCol, on="color", how = "left", validate = "many_to_one")
+        faceC["arch"] = "upper"
+    elif upperColCount == 1:
+        faceC = faceC.merge(lNumCol, on="color", how = "left", validate = "many_to_one")
+        faceC["arch"] = "lower"
+    else:
+        ValueError("could not identify input as upper or lower arch")
+    #return updated dataframe
+    return faceC
+#example
+# import os
+# os.chdir("H:\\schoolFiles\\dissertation\\intraoralSegmentation\\fastTgcn\\data\\train-Lall")
+# l76 = plyRead("076_L.ply")
+# l76["face"] = toothNum(l76["face"])
+
+
+#plot face and vertex dataframes, works with above but also raw reads from PlyData
 #takes the ply data as data frames so you can manipulate it beforehand
 #faces is a df of the faces
 #vertices is a df of the vertices
@@ -45,13 +114,13 @@ def plotPly(face, vertex):
     return surf.plot(scalars = "rgba", rgb = True)
 
 #example
+#example
 # import os
-# from plyfile import PlyData
 # os.chdir("H:\\schoolFiles\\dissertation\\intraoralSegmentation\\fastTgcn\\data\\train-Lall")
-# l76 = PlyData.read("076_L.ply")
-# l76Vert = pd.DataFrame(l76["vertex"].data)
-# l76Face = pd.DataFrame(l76["face"].data)
-# plotPly(face = l76Face, vertex = l76Vert)
+# l76 = plyRead("076_L.ply")
+# l76["face"] = toothNum(l76["face"])
+# plotPly(face = l76["face"], vertex = l76["vert"])
+
 
 
 
