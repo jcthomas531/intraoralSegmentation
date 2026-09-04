c3a2ffd844a969ec0403ad4220c28ebb4b2c95c5
J C C Thomas <jthomas48@argon-itf-login-3.hpc>
2026-09-04 10:20:08 -0500

    out of the box xgb predictions

--- rugaeDetect/firstXGB.py
+++ rugaeDetect/firstXGB.py
@@ -30,6 +30,16 @@ dAll = pd.concat([d1, d2])
 
 
 # https://www.youtube.com/watch?v=GrJP9FLV3FE
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import balanced_accuracy_score, roc_auc_score, make_scorer
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay
+
+rSeed = 826
+
 X = dAll.drop(columns=["label", "pat"]).copy()
 y = dAll["label"].copy()
 
@@ -37,3 +47,123 @@ y = dAll["label"].copy()
 X.dtypes.unique()
 X.dtypes.value_counts()
 y.dtypes
+y.unique()
+
+#split test train
+#unbalanced in labeled vs unlabeled so when we split a training and a test
+#set we will stratify on y so that we can maintain that percentage
+sum(y)/len(y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=rSeed, stratify=y)
+#check to make sure stratification worked
+sum(y_train)/len(y_train)
+sum(y_test)/len(y_test)
+
+#build xgboost model
+#note that this is pretty vanilla, not specifying anythin like depth or learning rate
+#see the video for how to optimize these hyperparams
+#note that i beleive that xgboost will export the last model, not the best model at the end
+#parameter scale_pos_weight helps with unbalanced data, adds penalty for incorrectly classifying minority class
+#tutorial also shows how to draw a tree, skipping for now
+clf_xgb = xgb.XGBClassifier(
+    objective = "binary:logistic",
+    seed = rSeed,
+    early_stopping_rounds = 10, #validation metric must improve at least once every X trees to continue training
+    eval_metric = "aucpr"  
+    )
+clf_xgb.fit(
+    X_train, 
+    y_train, 
+    verbose = True,
+    eval_set = [(X_test, y_test)]
+    )
+
+#see performance on test set
+testPred = clf_xgb.predict(X_test)
+cm = confusion_matrix(y_test, testPred, labels = clf_xgb.classes_)
+cmDisp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=clf_xgb.classes_)
+cmDisp.plot()
+
+#see performance on a new mouth
+d3 = pd.read_csv(inDir + "preLabeledCsv/pat007Pre_localDescrLabel.csv")
+d3["pat"] = "007"
+XNew = d3.drop(columns=["label", "pat"]).copy()
+yNew = d3["label"].copy()
+#data checking
+XNew.dtypes.unique()
+XNew.dtypes.value_counts()
+yNew.dtypes
+yNew.unique()
+#run through model
+predNew = clf_xgb.predict(XNew)
+cmNew = confusion_matrix(yNew, predNew, labels=clf_xgb.classes_)
+cmDispNew = ConfusionMatrixDisplay(confusion_matrix=cmNew, display_labels=clf_xgb.classes_)
+cmDispNew.plot()
+#predict class probability
+predProbNew = clf_xgb.predict_proba(XNew)
+predProb0New = predProbNew[:,0]
+predProb1New = predProbNew[:,1]
+newDf = pd.DataFrame(yNew)
+newDf["pred"] = predNew
+newDf["predProb0"] = predProb0New
+newDf["predProb1"] = predProb1New
+
+#auc for new mouth
+from sklearn import metrics
+fpr, tpr, thresholds = metrics.roc_curve(newDf["label"], newDf["predProb1"])
+metrics.auc(fpr, tpr)
+
+#plot and see what it looks like
+d3["predLabel"] = predNew
+d3["rugaeComparison"] = np.select(
+    [
+        (d3["label"] == 1) & (d3["predLabel"] == 1),
+        (d3["label"] == 1) & (d3["predLabel"] == 0),
+        (d3["label"] == 0) & (d3["predLabel"] == 1),
+        (d3["label"] == 0) & (d3["predLabel"] == 0)
+
+    ],
+    [
+        1,
+        2,
+        3,
+        4
+    ],
+    default = 0
+)
+#plot just the predicted labels
+import matplotlib.pyplot as plt
+%matplotlib widget
+colors = d3["predLabel"].map({
+    0: "grey",
+    1: "deeppink"
+})
+fig = plt.figure()
+ax = fig.add_subplot(111, projection="3d")
+ax.scatter(
+    d3["x"],
+    d3["y"],
+    d3["z"],
+    c=colors,
+    s=5
+)
+plt.show()
+
+
+#plot rugae comparison
+%matplotlib widget
+colors2 = d3["rugaeComparison"].map({
+    1: "green",
+    2: "blue",
+    3: "orange",
+    4: "grey"
+})
+fig2 = plt.figure()
+ax2 = fig2.add_subplot(111, projection="3d")
+ax2.scatter(
+    d3["x"],
+    d3["y"],
+    d3["z"],
+    c=colors2,
+    s=5
+)
+plt.show()
